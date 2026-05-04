5beac6552252094b6ef3ef399ad0df78ad59cb6a
Wyatt Puff <puffwya000@gmail.com>
2026-05-04 15:38:51 -0400

    add automatic DB migration on startup

--- WorkflowHub.API/Program.cs
+++ WorkflowHub.API/Program.cs
@@ -114,6 +114,12 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Admin seeding
 using (var scope = app.Services.CreateScope())
 {
