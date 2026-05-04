0a3cac74fa915e5172b34a86d34ad8106e7c26bb
Wyatt Puff <puffwya000@gmail.com>
2026-05-04 12:23:27 -0400

    switched to HashRouter for github pages

--- workflowhub-frontend/src/App.js
+++ workflowhub-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { getUserRole } from "./auth";
 import LoginPage from "./pages/LoginPage";
 import Register from "./pages/Register";
@@ -18,73 +18,71 @@ import AdminUsers from "./components/AdminUsers";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
+    <Routes>
 
-        {/* public route */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<Register />} />
+      {/* public route */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<Register />} />
 
-        {/* protected app */}
-        <Route path="/" element={<MainLayout />}>
-          
-          <Route index element={<Navigate to="/dashboard" />} />
+      {/* protected app */}
+      <Route path="/" element={<MainLayout />}>
+        
+        <Route index element={<Navigate to="/dashboard" />} />
 
-          <Route
-            path="dashboard"
-            element={
-              <ProtectedRoute>
-                <DashboardPage />
-              </ProtectedRoute>
-            }
-          />
+        <Route
+          path="dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
 
-          <Route path="/projects" element={<Projects />} />
+        <Route path="projects" element={<Projects />} />
 
-          <Route path="/projects/new" element={<ProjectCreate />} />
+        <Route path="projects/new" element={<ProjectCreate />} />
 
-          <Route path="/projects/:id" element={<ProjectDetail />} />
+        <Route path="projects/:id" element={<ProjectDetail />} />
 
-          <Route
-            path="tasks"
-            element={
-              <ProtectedRoute>
-                <TasksPage />
-              </ProtectedRoute>
-            }
-          />
+        <Route
+          path="tasks"
+          element={
+            <ProtectedRoute>
+              <TasksPage />
+            </ProtectedRoute>
+          }
+        />
 
-          <Route
-            path="tasks/new"
-            element={
-              <ProtectedRoute>
-                <TaskCreatePage />
-              </ProtectedRoute>
-            }
-          />
+        <Route
+          path="tasks/new"
+          element={
+            <ProtectedRoute>
+              <TaskCreatePage />
+            </ProtectedRoute>
+          }
+        />
 
-          <Route
-            path="/activity"
-            element={
-              getUserRole() === "Admin" ? (
-                <ActivityLogs />
-              ) : (
-                <Navigate to="/dashboard" />
-              )
-            }
-          />
+        <Route
+          path="activity"
+          element={
+            getUserRole() === "Admin" ? (
+              <ActivityLogs />
+            ) : (
+              <Navigate to="/dashboard" />
+            )
+          }
+        />
 
-          <Route
-            path="/admin/users"
-            element={
-              getUserRole() === "Admin" ? <AdminUsers /> : <Navigate to="/dashboard" />
-            }
-          />
+        <Route
+          path="admin/users"
+          element={
+            getUserRole() === "Admin" ? <AdminUsers /> : <Navigate to="/dashboard" />
+          }
+        />
 
-        </Route>
+      </Route>
 
-      </Routes>
-    </BrowserRouter>
+    </Routes>
   );
 }
 
