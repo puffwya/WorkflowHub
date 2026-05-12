30811923d37aaac795b299a0ff00cf513175b6dc
Wyatt Puff <puffwya000@gmail.com>
2026-05-12 15:38:15 -0400

    test jwt key

--- WorkflowHub.API/Program.cs
+++ WorkflowHub.API/Program.cs
@@ -33,6 +33,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // --------------------
 var jwtKey = builder.Configuration["Jwt:Key"];
 
+Console.WriteLine($"JWT KEY: '{builder.Configuration["Jwt:Key"]}'");
+
 if (string.IsNullOrEmpty(jwtKey))
 {
     throw new Exception("JWT Key is missing. Set Jwt:Key in environment variables.");
